1d2a24c5a23124042d0f8e75bef55dbfd190230d
Subhaharini <subhaharini.act@gmail.com>
2026-07-21 21:55:44 +0530

    practice

--- React-Native/MyAppBlank/App.tsx
+++ React-Native/MyAppBlank/App.tsx
@@ -1,203 +1,98 @@
-import { StyleSheet, Text, View, Image, Button, Pressable, TextInput, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, ScrollView, FlatList } from 'react-native';
+import {
+  StyleSheet, Text, View, Image, Button,
+  Pressable, TextInput, TouchableOpacity,
+  TouchableHighlight, TouchableWithoutFeedback,
+  ScrollView, FlatList, SectionList,
+  Switch, ActivityIndicator
+} from 'react-native';
 import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+function HomeScreen({ navigation }) {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Button
+        title='Go to Profile'
+        onPress={() => navigation.navigate("Profile", {
+          userName: "Subha",
+          age: 22,
+        })}
+      />
+    </View>
+  );
+}
+
+function ProfileScreen({ navigation, route }) {
+  const { userName, age } = route.params;
+  navigation.setOptions({
+    title:userName,
+  });
+  return (
+    <View style={{ gap: 5 }}>
+      <Text>Profile Screen</Text>
+      <Text>Welcome {userName}</Text>
+      <Text>Age: {age}</Text>
+      <Button
+        title='Go to Setting'
+        onPress={() => navigation.navigate("Setting")}
+      />
+      <Button
+        title='Go Back to Home Screen'
+        onPress={() => navigation.goBack()}
+      />
+
+    </View>
+  );
+}
+
+function SettingScreen({ navigation }) {
+  return (
+    <View>
+      <Text>Setting Screen</Text>
+      <Button
+        title='Go Back to Profile'
+        onPress={() => navigation.goBack()}
+      />
+    </View>
+  )
+}
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [name, setName] = useState("");
-  const languages = [
-    "Java",
-    "JavaScript",
-    "React",
-    "React Native",
-    "SQL",
-    "MongoDB",
-  ];
-  const skills = [
-    { id: "1", name: "Java" },
-    { id: "2", name: "React" },
-    { id: "3", name: "SQL" },
-    { id: "4", name: "MongoDB" },
-  ];
-  const students = [
-    "Subha",
-    "Rahul",
-    "Priya",
-    "Arun",
-  ];
+
   return (
-    // <View style={styles.card} >
-    //   {/* <Text >😀</Text>
-    //   <Text >😎</Text>
-    //   <Text>🚀</Text> */}
-    //   <Image source={require("./assets/people.png")} style={styles.img} />
-    //   <Text>Name : Subha Harini </Text>
-    //   <Text>Role : Frontend Developer</Text>
-    //   <Text>City : Salem </Text>
-    //   <Pressable style={({ pressed }) => ({
-    //     backgroundColor: pressed ? "#008000" : "#32cd32",
-    //     paddingHorizontal: 8,
-    //     paddingVertical: 8,
-    //     borderRadius: 10,
-    //   })}>
-    //     <Text style={styles.editText}>Edit Profile</Text>
-    //   </Pressable>
-    //   <Pressable style={({ pressed }) => ({
-    //     backgroundColor: pressed ? "#ff0000" : "#ff4500",
-    //     paddingHorizontal: 20,
-    //     paddingVertical: 8,
-    //     borderRadius: 10,
-    //   })}>
-    //     <Text style={styles.editText}>Logout</Text>
-    //   </Pressable>
-    // </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            // title:"Dashboard"
+            // headerShown: false,
+            headerTintColor: "#fff",
+            headerTitleAlign: "center",
 
-    // <View style={styles.content}>
-    //   <Text>Name</Text>
-    //   <TextInput
-    //     placeholder='Enter your name'
-    //     value={name}
-    //     onChangeText={setName}
-    //     style={styles.input}
-    //     keyboardType='default'
-    //   />
-    //   <Text>"Hello, {name}!"</Text>
-    //   <Text>Email</Text>
-    //   <TextInput
-    //     placeholder='Enter your email'
-    //     style={styles.input}
-    //     keyboardType='email-address'
-    //   />
-    //   <Text>Password</Text>
-    //   <TextInput
-    //     placeholder='Enter your password'
-    //     style={styles.input}
-    //     secureTextEntry={true}
-    //     keyboardType='default'
-    //   />
-    // </View>
+            headerStyle: {
+              backgroundColor: "#2563EB",
 
-    // <View style={styles.content}>
-    //   <TouchableOpacity style={styles.button} onPress={() => alert("Pressed")}>
-    //     <Text style={styles.text}>Login</Text>
-    //   </TouchableOpacity>
-    //   <TouchableHighlight
-    //     underlayColor="red"
-    //     style={styles.button}
-    //     onPress={() => alert("Pressed")}>
-    //     <Text style={styles.text}>Login</Text>
-    //   </TouchableHighlight>
-    //   <TouchableOpacity style={styles.button} onPress={() => alert("Pressed")}>
-    //     <Text style={styles.text}>Login</Text>
-    //   </TouchableOpacity>
-    // </View>
-    // <ScrollView
-    //   contentContainerStyle=
-    //   {{
-    //     gap: 60, padding: 10
-    //   }}
-    //   showsHorizontalScrollIndicator={false}
-    //   horizontal={true}
-    // >
-    //   <Text>My Favorite Movies</Text>
-    //   <Text>🎬 Interstellar</Text>
-    //   <Text>🎬 Avengers</Text>
-    //   <Text>🎬 Your Name</Text>
-    //   <Text>🎬 Spirited Away</Text>
-    //   <Text>🎬 Demon Slayer</Text>
-    //   <Text>🎬 Inception</Text>
-    //   <Text>🎬 Naruto</Text>
-    //   <Text>🎬 One Piece</Text>
-    //   <Text>🎬 Attack on Titan</Text>
-    //   <Text>🎬 Jujutsu Kaisen</Text>
-
-    // </ScrollView>
-    // <FlatList
-    //   data={languages}
-    //   renderItem={({ item }) => (
-    //     <Text style={styles.text}>{item}</Text>
-    //   )}
-    // />
-    // <FlatList
-    // data={skills}
-    // numColumns={2}
-    // keyExtractor={(item)=>item.id}
-    // renderItem={({item})=>(
-    //   <Text style={styles.text}>{item.name}</Text>
-    // )}
-    // />
-    <FlatList
-      data={students}
-      keyExtractor={(item, index) => index.toString()}
-      ListHeaderComponent={
-        <Text style={styles.header}>Student List</Text>
-      }
-      ListFooterComponent={
-        <Text style={styles.footer}>End of Students</Text>
-      }
-      ItemSeparatorComponent={() => (
-        <View
-          style={{
-            backgroundColor: "green",
-                        
+            }
           }}
         />
-      )}
-      renderItem={({ item }) => (
-        <Text style={styles.text}>{item}</Text>
-      )}
-    />
-
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+        />
+        <Stack.Screen
+          name='Setting'
+          component={SettingScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  content: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    gap: 10
-  },
-  input: {
-    borderWidth: 2,
-    width: 250,
-    borderRadius: 5,
-    padding: 8
-  },
-  button: {
-    backgroundColor: "green",
-    padding: 15,
-    borderRadius: 10,
-    alignItems: "center",
-  },
 
-  text: {
-    backgroundColor: "green",
-    textAlign: "center",
-    color: "white"
-
-  },
-  header: {
-    padding: 20,
-    textAlign: "center"
-  },
-  footer: {
-    padding: 20,
-    textAlign: "center"
-  }
-
-  // card: {
-  //   flex: 1,
-  //   borderWidth: 3,
-  //   borderColor: "red",
-  //   justifyContent: "center",
-  //   alignItems: "center",
-  //   gap: 10
-  // },
-  // img: {
-  //   width: 150,
-  //   height: 150,
-  //   borderRadius: 75,
-  // },
-  // editText: {
-  //   color: "white",
-  // },
 })
