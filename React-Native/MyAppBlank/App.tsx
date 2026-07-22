9df63ab1f680f34d3ac58bdf8a13436a4e1e0074
Subhaharini <subhaharini.act@gmail.com>
2026-07-22 21:52:15 +0530

    practice

--- React-Native/MyAppBlank/App.tsx
+++ React-Native/MyAppBlank/App.tsx
@@ -5,94 +5,83 @@ import {
   ScrollView, FlatList, SectionList,
   Switch, ActivityIndicator
 } from 'react-native';
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-function HomeScreen({ navigation }) {
-  return (
-    <View>
-      <Text>Home Screen</Text>
-      <Button
-        title='Go to Profile'
-        onPress={() => navigation.navigate("Profile", {
-          userName: "Subha",
-          age: 22,
-        })}
-      />
-    </View>
-  );
-}
-
-function ProfileScreen({ navigation, route }) {
-  const { userName, age } = route.params;
-  navigation.setOptions({
-    title:userName,
-  });
-  return (
-    <View style={{ gap: 5 }}>
-      <Text>Profile Screen</Text>
-      <Text>Welcome {userName}</Text>
-      <Text>Age: {age}</Text>
-      <Button
-        title='Go to Setting'
-        onPress={() => navigation.navigate("Setting")}
-      />
-      <Button
-        title='Go Back to Home Screen'
-        onPress={() => navigation.goBack()}
-      />
-
-    </View>
-  );
-}
-
-function SettingScreen({ navigation }) {
-  return (
-    <View>
-      <Text>Setting Screen</Text>
-      <Button
-        title='Go Back to Profile'
-        onPress={() => navigation.goBack()}
-      />
-    </View>
-  )
-}
-const Stack = createNativeStackNavigator();
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
 
+  async function saveData() {
+    const user = {
+      name,
+      age,
+    };
+    const jsonUser = JSON.stringify(user);
+    await AsyncStorage.setItem("user", jsonUser)
+  }
+  async function readData() {
+    const response = await AsyncStorage.getItem("user");
+    if (response !== null) {
+      const data = JSON.parse(response);
+      console.log(data);
+      setName(data.name)
+      setAge(data.age)
+    }
+  }
+
+  async function deleteData() {
+    await AsyncStorage.removeItem("user");
+  }
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            // title:"Dashboard"
-            // headerShown: false,
-            headerTintColor: "#fff",
-            headerTitleAlign: "center",
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <TextInput
+        placeholder='Enter your name'
+        value={name}
+        onChangeText={setName}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder='Enter your age'
+        value={age}
+        onChangeText={setAge}
+        style={styles.input}
 
-            headerStyle: {
-              backgroundColor: "#2563EB",
+      />
+      <View style={styles.button}>
+        <Button
+          title='Save'
+          onPress={saveData}
+        />
+        <Button
+          title='Load'
+          onPress={readData}
+        />
+        <Button
+          title='Delete'
+          onPress={deleteData}
+        />
 
-            }
-          }}
-        />
-        <Stack.Screen
-          name="Profile"
-          component={ProfileScreen}
-        />
-        <Stack.Screen
-          name='Setting'
-          component={SettingScreen}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+      </View>
+      <Text>Name: {name}</Text>
+      <Text>Age: {age}</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  input: {
+    borderWidth: 2,
+    padding: 3,
+    margin: 3
+  },
+  button: {
+    gap: 2,
+    flexDirection: "row",
+  }
 })
